63fe3872b67ad2c21e9db8f5f01618709ff60f9a
rsaniei1991 <saniei.rana@gmail.com>
2025-03-19 01:45:19 +0100

    add the build command

--- model/database.js
+++ model/database.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
-
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 var file = process.argv[2];
 // console.log("file is: ", file);
@@ -12,23 +11,25 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.PORT;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "127.0.0.1",
-  user: DB_USER || "root",
-  password: DB_PASS,
-  database: DB_NAME,
-  multipleStatements: true
+	host: DB_HOST || "127.0.0.1",
+	user: DB_USER || "root",
+	password: DB_PASS,
+	database: DB_NAME,
+	port: DB_PORT,
+	multipleStatements: true,
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+con.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected!");
 
-  con.query(migrationSQL, function(err, result) {
-    if (err) throw err;
-    console.log("Migration was completed");
-  });
+	con.query(migrationSQL, function (err, result) {
+		if (err) throw err;
+		console.log("Migration was completed");
+	});
 
-  con.end();
+	con.end();
 });
